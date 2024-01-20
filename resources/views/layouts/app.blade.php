27c7d23c0e4ead108b3765cad068a0f4549bf017
jisungyoo <jisung87kr@gmail.com>
2024-01-20 10:04:34 +0900

    라우터, 기본레이아웃 설정

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,25 +17,110 @@
         <!-- Styles -->
         @livewireStyles
     </head>
-    <body class="font-sans antialiased">
+    <body class="font-sans antialiased" x-data>
         <x-banner />
 
-        <div class="min-h-screen bg-gray-100">
-            @livewire('navigation-menu')
+        <div class="flex flex-col min-h-screen min-h-screen bg-gray-100">
 
-            <!-- Page Heading -->
-            @if (isset($header))
-                <header class="bg-white shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}
+            <header class="p-5 flex container mx-auto justify-between items-baseline">
+                <div class="flex content-center items-baseline">
+                    <a href="/" class="mr-5 font-bold">REMON</a>
+                    <form action="{{ route('index') }}">
+                        <div class="relative">
+                            <input type="text"
+                                   name="search"
+                                   class="block w-full p-4 pe-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500"
+                                   placeholder="원하는 캠페인을 검색해보세요" value="{{ request()->input('search') }}">
+                            <div class="absolute inset-y-0 end-0 flex items-center pe-3">
+                                <button type="submit">
+                                    <svg class="w-4 h-4 text-gray-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                                         fill="none" viewBox="0 0 20 20">
+                                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                              d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
+                                    </svg>
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+                @if(Auth::check())
+                    <div class="ms-3 relative">
+                        <x-dropdown align="right" width="48">
+                            <x-slot name="trigger">
+                                @if (Laravel\Jetstream\Jetstream::managesProfilePhotos())
+                                    <button class="flex text-sm border-2 border-transparent rounded-full focus:outline-none focus:border-gray-300 transition">
+                                        <img class="h-8 w-8 rounded-full object-cover" src="{{ Auth::user()->profile_photo_url }}" alt="{{ Auth::user()->name }}" />
+                                    </button>
+                                @else
+                                    <span class="inline-flex rounded-md">
+                                    <button type="button" class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none focus:bg-gray-50 active:bg-gray-50 transition ease-in-out duration-150">
+                                        {{ Auth::user()->name }}
+
+                                        <svg class="ms-2 -me-0.5 h-4 w-4" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+                                            <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
+                                        </svg>
+                                    </button>
+                                </span>
+                                @endif
+                            </x-slot>
+
+                            <x-slot name="content">
+                                <!-- Account Management -->
+                                <div class="block px-4 py-2 text-xs text-gray-400">
+                                    {{ __('Manage Account') }}
+                                </div>
+
+                                <x-dropdown-link href="{{ route('mypage') }}">
+                                    {{ __('마이페이지') }}
+                                </x-dropdown-link>
+
+
+                                <div class="border-t border-gray-200"></div>
+
+                                <!-- Authentication -->
+                                <form method="POST" action="{{ route('logout') }}" x-data>
+                                    @csrf
+
+                                    <x-dropdown-link href="{{ route('logout') }}" @click.prevent="$root.submit();">
+                                        {{ __('로그아웃') }}
+                                    </x-dropdown-link>
+                                </form>
+                            </x-slot>
+                        </x-dropdown>
                     </div>
-                </header>
-            @endif
+                @else
+                    <div class="flex gap-1 items-baseline">
+                        <a href="{{ route('login') }}" class="text-gray-600 text-sm">로그인</a>
+                        <span>∙</span>
+                        <a href="{{ route('register') }}" class="text-gray-600 text-sm">회원가입</a>
+                    </div>
+                @endif
+            </header>
+            <div class="border-b">
+                <div class="container mx-auto p-5">
+                    <nav>
+                        <ul class="flex gap-5">
+                            <li class="font-bold"><a href="{{ route('category.index') }}">전체 카테고리</a></li>
+                            <li class="font-bold"><a href="{{ route('category.index') }}">오늘오픈</a></li>
+                            <li class="font-bold"><a href="{{ route('category.index') }}">제품별</a></li>
+                            <li class="font-bold"><a href="{{ route('category.index') }}">지역별</a></li>
+                            <li class="font-bold"><a href="{{ route('event') }}">이벤트</a></li>
+                            <li class="font-bold"><a href="{{ route('community.free') }}">커뮤니티</a></li>
+                            <li class="font-bold"><a href="{{ route('help.faq') }}">고객센터</a></li>
+                            <li class="font-bold"><a href="{{ route('help.contact') }}">광고문의</a></li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
 
             <!-- Page Content -->
-            <main>
+            <main class="grow min-h-full">
                 {{ $slot }}
             </main>
+
+            <footer>
+                footer
+            </footer>
         </div>
 
         @stack('modals')
--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,93 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
+})->name('index');
+
+Route::get('/campaigns/{campaign}', function($campaign){
+    return $campaign;
+})->name('campaigns.show');
+
+Route::get('/campaigns/{campaign}/apply', function($campaign){
+    return $campaign.'/apply';
+})->name('campaigns.apply');
+
+Route::get('/brandzone/{brandzone}', function($brandzone){
+    return $brandzone;
+})->name('brandzone.show');
+
+Route::get('/category', function($category){
+    return  $category;
+})->name('category.index');
+
+Route::get('/category/{category}', function($category){
+   return  $category;
+})->name('category.show');
+
+Route::get('/community/free', function(){
+    return 'community/free';
+})->name('community.free');
+
+Route::get('/community/guide', function(){
+    return 'community/guide';
+})->name('community.guide');
+
+Route::get('/community/neighbor', function(){
+    return 'community/neighbor';
+})->name('community.neighbor');
+
+Route::get('/event', function(){
+   return 'event';
+})->name('event');
+
+Route::get('/help/notice', function(){
+    return 'help/notice';
+})->name('help.notice');
+
+Route::get('/help/inquiry', function(){
+    return 'help/inquiry';
+})->name('help.inquiry');
+
+Route::get('/help/faq', function(){
+    return 'help.faq';
+})->name('help.faq');
+
+Route::get('/help/contact', function(){
+    return 'help.contact';
+})->name('help.contact');
+
+Route::prefix('/mypage')->name('maypage.')->group(function(){
+    Route::get('/campaigns', function(){
+        return '/maypage/campaigns';
+    })->name('campaigns');
+
+    Route::get('/reviews', function(){
+       return '/mypage/reviews';
+    })->name('reviews');
+
+    Route::get('/messages', function(){
+        return '/mypage/messages';
+    })->name('messages');
+
+    Route::get('/profile', function(){
+        return '/mypage/profile';
+    })->name('profile');
+
+    Route::get('/profile/information', function(){
+        return '/mypage/profile/information';
+    })->name('profile.information');
+
+    Route::get('/media', function(){
+        return '/mypage/media';
+    })->name('media');
+
+    Route::get('/penalty', function(){
+        return '/mypage/penalty';
+    })->name('penalty');
+
+    Route::get('/point', function(){
+        return '/mypage/point';
+    })->name('point');
 });
 
 Route::middleware([
