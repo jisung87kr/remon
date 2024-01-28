6f7a9c943c63d455251941f4711f3d8a49c62dfd
jisungyoo <jisung87kr@gmail.com>
2024-01-28 20:28:28 +0900

    테이블명 수정

--- database/migrations/2024_01_27_104646_create_campaign_mission_option_table.php
+++ database/migrations/2024_01_27_104646_create_campaign_mission_option_table.php
@@ -29,6 +29,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('campaign_mission_options');
+        Schema::dropIfExists('campaign_mission_option');
     }
 };
