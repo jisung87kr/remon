c835ba6f6b7bac3709447bc7c3b855ec917885b4
jisungyoo <jisung87kr@gmail.com>
2024-01-29 23:44:27 +0900

    캠페인 동의 필드 추가

--- database/migrations/2024_01_29_203351_create_campaign_applicants_table.php
+++ database/migrations/2024_01_29_203351_create_campaign_applicants_table.php
@@ -21,6 +21,8 @@ return new class extends Migration
             $table->enum('sex', ['man', 'woman'])->comment('신청자 성별');
             $table->string('phone')->comment('신청자 연락처');
             $table->string('status')->default(ApplicantStatus::APPLIED->name)->comment('신청상태');
+            $table->boolean('portrait_right_consent')->default(0)->comment('초상권 활용 동의');
+            $table->boolean('base_right_consent')->default(0)->comment('캠페인 유의사항, 개인정보 및 콘텐츠 제3자 제공, 저작물이용 동의');
             $table->timestamps();
         });
     }
