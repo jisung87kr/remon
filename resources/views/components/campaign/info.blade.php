24a1e022e1c56bab058b52e9de709e009067bc93
jisungyoo <jisung87kr@gmail.com>
2024-05-16 22:31:56 +0900

    변수 오류 수정

--- resources/views/components/campaign/info.blade.php
+++ resources/views/components/campaign/info.blade.php
@@ -16,20 +16,20 @@
             @endif
         </div>
     </div>
-    <div class="grid grid-cols-1 md:grid-cols-2 gap-3">
-        <div class="flex items-center">
+    <div class="grid grid-cols-1 lg:grid-cols-2 gap-3">
+        <div class="flex">
             <div class="mr-1 font-bold shrink-0 w-[120px]">캠페인 신청기간</div>
             <div class="text-gray-800">{{ $campaign->application_start_at->format('y.m.d') }} ~ {{ $campaign->application_end_at->format('y.m.d') }}</div>
         </div>
-        <div class="flex items-center">
+        <div class="flex">
             <div class="mr-1 font-bold shrink-0 w-[120px]">인플루언서 발표</div>
             <div class="text-gray-800">{{ $campaign->announcement_at->format('y.m.d') }}</div>
         </div>
-        <div class="flex items-center">
+        <div class="flex">
             <div class="mr-1 font-bold shrink-0 w-[120px]">컨텐츠 등록기간</div>
             <div class="text-gray-800">{{ $campaign->registration_start_date_at->format('y.m.d') }} ~ {{ $campaign->registration_end_date_at->format('y.m.d') }}</div>
         </div>
-        <div class="flex items-center">
+        <div class="flex">
             <div class="mr-1 font-bold shrink-0 w-[120px]">캼페인 종료일</div>
             <div class="text-gray-800">{{ $campaign->result_announcement_date_at->format('y.m.d') }}</div>
         </div>
